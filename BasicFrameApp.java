03ba41d0ac9d93f95fd4cc1b9ef71767a163e0ae
ramoshaba-k <kgothatsoramoshaba1@gmail.com>
2024-08-06 11:37:05 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,16 @@
+
+package za.ac.tut.ui;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+public class BasicFrameApp {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        MyFirstFrame myFrame =new MyFirstFrame();
+        
+    }
+    
+}
---
+++ MyFirstFrame.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package za.ac.tut.ui;
+
+import java.awt.Color;
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
+class MyFirstFrame extends JFrame{
+      public MyFirstFrame() {
+        setTitle("My first GUI");
+        setSize(200,250);
+        setDefaultLookAndFeelDecorated(true);
+        setResizable(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setForeground(Color.BLUE);
+        setVisible(true);
+    }
+}
